6bfbff5a0ff3319981db262bdb015c79dbcae26a
Drasko Gomboc <drasko.gomboc@gmail.com>
2017-03-29 09:33:33 +0200

    Extracted process() to Processor; added zend-config-aggregator as a dependency

--- src/Config.php
+++ src/Config.php
@@ -2,8 +2,6 @@
 
 namespace G4\Config;
 
-use Zend\Config\Reader\Ini as Reader;
-use G4\Log\Writer;
 
 class Config
 {
@@ -14,8 +12,6 @@ class Config
     private $data;
 
     private $path;
-
-    private $sections;
 
     private $section;
 
@@ -94,86 +90,9 @@ class Config
 
     private function process()
     {
-        $path = realpath($this->path);
-
-        if(false === $path || !is_readable($path)) {
-            throw new \Exception('Configuration file is not readable');
-        }
-
-        $reader = new Reader();
-        $this->data = $reader->fromFile($path);
-
-        $this->data = null !== $this->section
-            ? $this->getSection($this->section)
-            : $this->mergeSections();
+        $this->data = (new Processor($this->path, $this->section))->process();
 
         return $this;
-    }
-
-    private function getSection($name)
-    {
-        $this->processSections();
-
-        if(!array_key_exists($name, $this->sections)) {
-            throw new \Exception("Section '{$name}' missing");
-        }
-
-        $extends = false;
-        $parentData = array();
-
-        if($this->sections[$name] !== null) {
-            $extends = true;
-            $func = __FUNCTION__;
-            $parentData = $this->$func($this->sections[$name]);
-        }
-
-        $sectionName = $extends === true
-            ? $name . ':' . $this->sections[$name]
-            : $name;
-
-        $sectionData = $this->data[$sectionName];
-
-        $newData = array_replace_recursive($parentData, $sectionData);
-
-        ksort($newData);
-
-        return $newData;
-    }
-
-    private function processSections()
-    {
-        $tmp = array_keys($this->data);
-
-        foreach($tmp as $item) {
-            if(substr_count($item, ":") > 1) { }
-
-            $segments = explode(':', $item);
-
-            $first = trim(array_shift($segments));
-
-            $this->sections[$first] = !empty($segments)
-                ? trim(array_shift($segments))
-                : null;
-        }
-
-        return $this;
-    }
-
-    private function getSectionName($item)
-    {
-        if(substr_count($item, ":") > 1) { }
-        $segments = explode(':', $item);
-        return $segments[0];
-    }
-
-    private function mergeSections()
-    {
-        $tmpData = [];
-        foreach($this->data as $sectionName => $data){
-            $name = $this->getSectionName($sectionName);
-            $tmpData[$name] = $this->getSection($name);
-        }
-        return $tmpData;
     }
 
     public function getData($force = false)
